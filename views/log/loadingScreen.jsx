e209b07f75a528f2e2b9d591ca939b7ff3358ce8
=Bruno <=>
2025-11-03 18:20:32 -0300

    feat: Add user avatar and chat item component
    - Introduced a new ChatItem component for displaying user chat items with an avatar and username.
    - Added RollingBorder component for animated border effects.
    - Implemented a TabBar component with navigation icons for home, search, create, messages, and profile.
    - Created an IconLogo component using SVG for a base64 encoded image.
    - Added utility styles for consistent layout across components.
    - Developed a loading screen that incorporates a logo and the RollingBorder component.
    - Implemented ListChat view to render multiple ChatItem components.
    - Included a user avatar image in the assets.

---
+++ views/log/loadingScreen.jsx
@@ -0,0 +1,18 @@
+import { utility } from "@/style/index";
+import { Image, View } from "react-native";
+import Border from "../../components/Rollingborder";
+export default function loadingScreen() {
+  return (
+    <View style={[utility.container, { gap: 75 }]}>
+      <Image
+        source={require("../../assets/image/logo.png")}
+        style={{
+          width: 380,
+          height: 220,
+          resizeMode: "contain",
+        }}
+      />
+      <Border />
+    </View>
+  );
+}
---
+++ views/message/chat/ListChat.jsx
@@ -0,0 +1,11 @@
+import { View } from "react-native";
+import ChatItem from "@/components/ChatItem";
+
+export default function ListChat() {
+  return (
+    <View>
+      <ChatItem />
+      <ChatItem />
+    </View>
+  );
+}
